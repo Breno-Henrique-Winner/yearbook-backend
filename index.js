fbaeac1a408bfcc43ec8967374a278b07b7009d1
Breno-Henrique-Ferreira <bhfs@aluno.ifnmg.edu.br>
2026-08-06 18:48:23 +0000

    feat: configura CORS para o frontend em domínio separado

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import 'dotenv/config';                              // carrega .env — PRIMEIRA LINHA
 import logger from './middlewares/logger.js';      // importa o middleware de log
+import cors from 'cors';                              // novo import
 import tratarErro from './middlewares/erro.js';     // novo import
 import express from 'express';                // importa o Express
 import alunosRouter from './routes/alunos.js'; // importa o router de alunos <- NOVO
@@ -8,6 +9,7 @@ import mensagensRouter from './routes/mensagens.js'; // novo import
 const app = express();      // cria a aplicação Express
 const PORT = process.env.PORT || 3000;  // lê do .env, com fallback para 3000
 
+app.use(cors());            // 1º — libera CORS para qualquer origem
 app.use(express.json());    // middleware que parseia JSON do body das requisições  <- NOVO
 app.use(logger);            // 2º — registra log de cada requisição
 
